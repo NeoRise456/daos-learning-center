35aeda9624d3dbe443687752ae10b40383b96ce3
neo <neorise456@gmail.com>
2024-09-16 10:39:05 -0500

    feat: monse

---
+++ src/app/learning/components/student-create-and-edit/student-create-and-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-create-and-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-create-and-edit.component.html',
+  styleUrl: './student-create-and-edit.component.css'
+})
+export class StudentCreateAndEditComponent {
+
+}
